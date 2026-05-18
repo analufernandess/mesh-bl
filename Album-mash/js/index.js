f99cdec04d623eeb2a2700577e7e37594ebe8ba6
analufernandess <40441@raphaeldisanto.com.br>
2026-05-18 08:51:40 -0300

    Add files via upload

---
+++ Album-mash/js/index.js
@@ -0,0 +1,50 @@
+//Lista de albuns com nome e imagem local
+const albumMusica = [
+    { nome: "Hit Me Hard And Soft", imagem: "img/BL-3.jpg"},
+    { nome: "Happier Than Ever", imagem: "img/BL-4.jpg"},
+    { nome: "When We All Fall Asleep, Where Do We Go?", imagem: "img/BL-2.jpg"},
+    { nome: "Live at Third Man Records", imagem: "img/BL-6.jpg"},
+    { nome: "Guitar Songs", imagem: "img/BL-5.jpg"},
+    { nome: "Don't Smile at Me", imagem: "img/BL-1.jpg"},
+];
+
+//Começa todos os votos zerados
+const votos = new Array(albumMusica.lenght).fill(0);
+
+//Guarda local que está sendo exibido agora 
+let indice1, indicece2;
+
+//Seleciona os elementos da página
+const cartao1 = document.getElementById("cartao1");
+const cartao2 = document.getElementById("cartao2");
+const resultado = document.getElementById("resultado");
+const barras = document.getElementById("barras");
+const avisoProx = document.getElementById("proximo-aviso");
+
+//Sorteia um novo par de looks do Met Gala diferentes
+function sortearPar(){
+    indice1 = Math.floor(Math.random() * metGala.length);
+    do{
+        indice2 = Math.floor(Math.random() * redesSociais.length);
+   } while (indice2 === indice1);
+}
+
+//atualiza cartão 1
+cartao1.querySelector("img").scr = metGala[indice1].imagem;
+cartao2.querySelector("img").scr = textContent[indice2].imagem;
+
+//atualiza cartão 2
+cartao2.querySelector("img").scr = metGala[indice2].imagem;
+cartao2.querySelector("img").scr = textContent[indice2].imagem;
+
+//esconde o resultado da rodada anterior
+resultado.style.display = "none";
+
+//habilita os cliques nos cartões
+cartao1.style.pointerEvents = "auto";
+cartao2.style.pointerEvents = "auto";
+
+//registra o voto e mostra o placar por 3 segundos
+function votar(indiceVencedor)(
+    votos[indiceVencedor]++;
+)
